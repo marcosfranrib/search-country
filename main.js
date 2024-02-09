c88bfe032a613655b23072d999112c7f0bed6ff6
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2024-02-09 16:52:03 -0300

    update

--- main.js
+++ main.js
@@ -36,13 +36,14 @@ document.addEventListener('DOMContentLoaded',()=>{
 })
 
 input.addEventListener('input',()=>{
-    let filter = input.value
+    let searchBar = input.value
     let cards = document.querySelectorAll('.card');
+    let optionValue = select.value.toLowerCase()
 
-    if (filter !='' && optionValue == 'qualquer região') {
+    if (searchBar !='' && optionValue == 'qualquer região') {
         for (let card of cards) {
             let h3 = card.querySelector('h3').textContent.toLowerCase()
-            let inputValue = filter.toLowerCase()
+            let inputValue = searchBar.toLowerCase()
             
             if (!h3.includes(inputValue)) {
                 card.style.display = 'none'
@@ -52,10 +53,19 @@ input.addEventListener('input',()=>{
             }
         }
 
-    } else if(filter !='' && optionValue != 'qualquer região'){
+    } else if(optionValue != 'qualquer região'){
+        // quando der o input, ele precisa pesquisr apenas nos que estão com a região igual a que está no option value
+
         for (let card of cards) {
+            let region = card.querySelector('.region').textContent.toLowerCase()
+            let h3 = card.querySelector('h3').textContent.toLowerCase()
+            let inputValue = searchBar.toLowerCase()
             
-    
+            if (optionValue == region && h3.includes(inputValue)) {
+                card.style.display = 'block'
+            } else{
+                card.style.display = 'none'   
+            }
         }
     }
 })
